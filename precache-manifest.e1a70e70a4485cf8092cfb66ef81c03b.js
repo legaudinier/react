a1b47b4d1429d1d56250e6d2132b81eb8f5615ff
Lindsay Gaudinier <lindsay@hook42.com>
2019-05-14 23:12:01 -0700

    Updates

---
+++ precache-manifest.e1a70e70a4485cf8092cfb66ef81c03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "8497abae1aa7fb886200",
+    "url": "/react/static/js/main.94b84aa6.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/react/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "8497abae1aa7fb886200",
+    "url": "/react/static/css/main.4f417d21.chunk.css"
+  },
+  {
+    "revision": "62ff3a4efa7e9142d15cab48b7f848df",
+    "url": "/react/index.html"
+  }
+];
